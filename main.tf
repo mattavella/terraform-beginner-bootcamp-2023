183e311c88c8637208b2593e4264fe5ce3f7558d
Matt Avella <azmattav@gmail.com>
2023-10-01 22:13:01 +0000

    #17 changed the structure or root module

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-terraform {
-  cloud {
-    organization = "mattavella"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-provider "aws" {
-}
-
-provider "random" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   length = 32
@@ -41,9 +12,8 @@ resource "aws_s3_bucket" "example" {
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
 
   bucket = random_string.bucket_name.result
-}
 
-
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
---
+++ providers.tf
@@ -0,0 +1,28 @@
+terraform {
+#  cloud {
+#    organization = "mattavella"
+
+#    workspaces {
+#      name = "terra-house-1"
+#    }
+#  }
+
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
+
+provider "aws" {
+}
+
+provider "random" {
+  # Configuration options
+}
