5633c913d228585109716f30d71af2a7e603190d
Matt Avella <azmattav@gmail.com>
2023-10-12 16:00:02 -0700

    #47 set up two towns and fix errors (#48)

--- main.tf
+++ main.tf
@@ -6,13 +6,12 @@ terraform {
     }
   }
   
-#  cloud {
-#    organization = "mattavella"
-
-#    workspaces {
-#      name = "terra-house-1"
-#    }
-#  }
+  cloud {
+    organization = "mattavella"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
 
 }
 
@@ -22,22 +21,39 @@ provider "terratowns" {
   token = var.terratowns_access_token
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
+module "home_ribeye_hosting" {
+  source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
   bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+  public_path = var.ribeye.public_path
+  content_version = var.ribeye.content_version
 }
 
 resource "terratowns_home" "home" {
   name = "How to cook the perfect ribeye steak"
   description = <<DESCRIPTION
-destription goes here
+This rib eye steak recipe is tender and juicy beef seared to golden brown perfection, and topped with garlic butter and herbs. A simple steak preparation that tastes like it came from a fancy restaurant!
 DESCRIPTION
-  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = module.home_ribeye_hosting.domain_name
   town = "missingo"
-  content_version = 1
+  content_version = var.ribeye.content_version
 }
+
+
+#module "home_brisket_hosting" {
+#  source = "./modules/terrahome_aws"
+#  user_uuid = var.teacherseat_user_uuid
+#  public_path = var.brisket.public_path
+#  content_version = var.brisket.content_version
+#
+#}
+#
+#resource "terratowns_home" "home" {
+#  name = "How to cook the perfect brisket"
+#  description = <<DESCRIPTION
+#This brisket recipe is will make the best, tender and juicy briskit you've ever had. 
+#DESCRIPTION
+#  domain_name = module.home_brisket_hosting.domain_name
+#  town = "missingo"
+#    content_version = var.brisket.content_version
+#}
