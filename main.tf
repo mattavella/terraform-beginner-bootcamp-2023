2da0740014c619c90fbd88fc49a0e1362d9c8d5f
Matt Avella <azmattav@gmail.com>
2023-10-12 01:47:07 +0000

    #45 fix of provider and TF errors

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
+  
 #  cloud {
 #    organization = "mattavella"
 
